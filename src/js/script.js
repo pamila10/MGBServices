8ed3b8067dfdfc94a6ce0dc84fe151fbc6dfea50
pamila10 <pmila1010@gmail.com>
2021-03-13 20:34:15 +0200

    add edits

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,7 @@ function addDropBtn() {
     }
     dropBtn.addEventListener('pointerenter', toggleBtn);
     // Close drop content
-    dropList.addEventListener('mouseleave', (e) => {
+    dropList.addEventListener(('mouseleave' || 'touchend'), (e) => {
         if (e.target.id === dropList) {
             dropContent.style.display = 'inline-block';
         } else {
